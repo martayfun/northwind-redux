f31e6d6c5897e4b39bbc61f9e73a5438fe3dd04c
Tayfun Ates <tayfun.ates@odeontechnology.com>
2022-05-16 18:18:51 +0300

    development update

--- src/components/IncreaseByTwoCounter.js
+++ src/components/IncreaseByTwoCounter.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import { increaseByTwoCounter } from '../redux/actions/counterActions'
 
-export default class IncreaseByTwoCounter extends Component {
+class IncreaseByTwoCounter extends Component {
     render() {
         return (
             <div>
-                
+                <button onClick={() => this.props.dispatch(increaseByTwoCounter())}>2 arttır</button>
             </div>
         )
     }
 }
+
+function mapDispatchToProps(dispatch) {
+    return { actions: bindActionCreators(increaseByTwoCounter, dispatch) }
+}
+
+export default connect(mapDispatchToProps)(IncreaseByTwoCounter);
